c44548c7193c9558c92faf15fcf4af4daf2d3c9e
trsskoBASH <semirkorda9@gmail.com>
2025-10-16 18:04:00 +0200

    Add dotnet migration

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310b6bb56168ab5cc595cb4f14377a9dd8f71554")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28106077aae7be90b304a494076833c04adc9ca8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
