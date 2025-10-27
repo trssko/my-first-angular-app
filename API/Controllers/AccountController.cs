34f53ce4da46eb95a8f1f9b07f25759fb200cb14
trsskoBASH <semirkorda9@gmail.com>
2025-10-27 17:24:41 +0100

    Add loginDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -21,12 +21,29 @@ public class AccountController(AppDbContext context) : BaseApiController
         {
             DisplayName = registerDTO.DisplayName,
             Mail = registerDTO.Mail,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Mail)),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
             PasswordSalt = hmac.Key
         };
 
         context.Users.Add(user);
         await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    [HttpPost("login")] // api/account/login
+    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO) {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Mail == loginDTO.Email);
+
+        if (user == null) return Unauthorized("Invalid email address");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for (var i = 0; i < computedHash.Length; i++) {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
 
         return user;
     }
