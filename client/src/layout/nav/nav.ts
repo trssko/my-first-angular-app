37b6789b2dd183d90eca924447d7e2bd3cd6094e
trsskoBASH <semirkorda9@gmail.com>
2025-11-05 14:30:48 +0100

    Add first service

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,5 +1,6 @@
- import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
@@ -9,8 +10,12 @@ import { FormsModule } from '@angular/forms';
 })
 export class Nav {
    protected creds: any = {}
+   private accountService = inject(AccountService);
 
    login() {
-    console.log(this.creds);
+    this.accountService.login(this.creds).subscribe({
+      next: result => console.log(result),
+      error: error => alert(error.message)
+    })
    }
 }
